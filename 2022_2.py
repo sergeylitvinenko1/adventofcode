93d43db39f60d5ed77d2f70219859acda4bed6d4
sergeylitvinenko1 <113466645+sergeylitvinenko1@users.noreply.github.com>
2023-10-16 16:23:45 +0200

    first commit

---
+++ 2022_2.py
@@ -0,0 +1,22 @@
+elfs = []
+
+with open ("2022_1_data.txt") as file: 
+    elfs = file.readlines()
+
+elfs = [item.strip("\n") for item in elfs]
+
+results = []
+temp = 0
+
+for item in elfs: 
+    if len(item) != 0: 
+        temp = temp + int(item)
+    else:
+        results.append(temp)
+        temp = 0
+        continue
+
+results.sort(reverse=True)
+
+top_3 = sum(results[0:3])
+print(top_3)
